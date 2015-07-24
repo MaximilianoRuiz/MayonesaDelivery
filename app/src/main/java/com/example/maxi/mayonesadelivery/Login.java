99c6f27a11f15e58103194653bb4c3a4939b6ee3
Maxi <maxi_ruiz140@hotmail.com>
2015-07-24 20:12:00 -0300

    Initial commit

---
+++ app/src/main/java/com/example/maxi/mayonesadelivery/Login.java
@@ -0,0 +1,46 @@
+package com.example.maxi.mayonesadelivery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class Login extends ActionBarActivity {
+    EditText user, pass;
+    Button login;
+    Activity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        activity = this;
+
+        user = (EditText) findViewById(R.id.user);
+        pass = (EditText) findViewById(R.id.pass);
+        login = (Button) findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (user.getText().toString().equals("super")){
+                    if (!pass.getText().toString().equals("")){
+                        Intent intent = new Intent(getBaseContext(), PrincipalMenu.class);
+                        startActivity(intent);
+                        activity.finish();
+                    }
+                } else {
+                    Toast.makeText(getBaseContext(), "Invalid User", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+
+}
